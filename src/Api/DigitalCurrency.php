a91ec5f6c3078e86973c387ce4a79126523efa59
Jonas Kraska <jonas.kraska@gmail.com>
2021-08-08 14:50:36 +0200

    updated/added missing endpoints for digital currencies and increased test coverage (format key not allowed in those demo calls)

--- src/Api/DigitalCurrency.php
+++ src/Api/DigitalCurrency.php
@@ -6,19 +6,23 @@ namespace AlphaVantage\Api;
 
 class DigitalCurrency extends AbstractApi
 {
+    public const INTERVAL_1 = '1min';
+    public const INTERVAL_5 = '5min';
+    public const INTERVAL_15 = '15min';
+    public const INTERVAL_30 = '30min';
+    public const INTERVAL_60 = '60min';
+
     /**
      * @param string $symbol
-     * @param string $market
-     * @param string $dataType
+     * @param null|string $dataType
      * @return array
      */
-    public function digitalCurrencyIntraday(string $symbol, string $market, string $dataType = self::DATA_TYPE_JSON)
+    public function digitalCurrencyRating(string $symbol, ?string $dataType = null)
     {
         return $this->get(
-            'DIGITAL_CURRENCY_INTRADAY',
+            'CRYPTO_RATING',
             $symbol,
             [
-                'market' => $market,
                 'datatype' => $dataType,
             ]
         );
@@ -27,10 +31,30 @@ class DigitalCurrency extends AbstractApi
     /**
      * @param string $symbol
      * @param string $market
-     * @param string $dataType
+     * @param string $interval
+     * @param null|string $dataType
      * @return array
      */
-    public function digitalCurrencyDaily(string $symbol, string $market, string $dataType = self::DATA_TYPE_JSON)
+    public function digitalCurrencyIntraday(string $symbol, string $market, string $interval, ?string $dataType = null)
+    {
+        return $this->get(
+            'CRYPTO_INTRADAY',
+            $symbol,
+            [
+                'market' => $market,
+                'interval' => $interval,
+                'datatype' => $dataType,
+            ]
+        );
+    }
+
+    /**
+     * @param string $symbol
+     * @param string $market
+     * @param null|string $dataType
+     * @return array
+     */
+    public function digitalCurrencyDaily(string $symbol, string $market, ?string $dataType = null)
     {
         return $this->get(
             'DIGITAL_CURRENCY_DAILY',
@@ -45,10 +69,10 @@ class DigitalCurrency extends AbstractApi
     /**
      * @param string $symbol
      * @param string $market
-     * @param string $dataType
+     * @param null|string $dataType
      * @return array
      */
-    public function digitalCurrencyWeekly(string $symbol, string $market, string $dataType = self::DATA_TYPE_JSON)
+    public function digitalCurrencyWeekly(string $symbol, string $market, ?string $dataType = null)
     {
         return $this->get(
             'DIGITAL_CURRENCY_WEEKLY',
@@ -63,10 +87,10 @@ class DigitalCurrency extends AbstractApi
     /**
      * @param string $symbol
      * @param string $market
-     * @param string $dataType
+     * @param null|string $dataType
      * @return array
      */
-    public function digitalCurrencyMonthly(string $symbol, string $market, string $dataType = self::DATA_TYPE_JSON)
+    public function digitalCurrencyMonthly(string $symbol, string $market, ?string $dataType = null)
     {
         return $this->get(
             'DIGITAL_CURRENCY_MONTHLY',
---
+++ tests/Api/DigitalCurrencyTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlphaVantageTest\Api;
+
+use AlphaVantage\Api\DigitalCurrency;
+
+class DigitalCurrencyTest extends TestCase
+{
+    public function testDigitalCurrencyIntraday()
+    {
+        $actual = (new DigitalCurrency($this->option))->digitalCurrencyIntraday('ETH', 'USD', DigitalCurrency::INTERVAL_5);
+
+        $this->assertIsArray($actual);
+        $this->assertCount(2, $actual);
+
+        $this->assertArrayHasKey('Meta Data', $actual);
+        $this->assertNotEmpty($actual['Meta Data']);
+
+        $this->assertArrayHasKey('Time Series Crypto (5min)', $actual);
+        $this->assertNotEmpty($actual['Time Series Crypto (5min)']);
+    }
+
+    public function testDigitalCurrencyDaily()
+    {
+        $actual = (new DigitalCurrency($this->option))->digitalCurrencyDaily('BTC', 'CNY');
+
+        $this->assertIsArray($actual);
+        $this->assertCount(2, $actual);
+
+        $this->assertArrayHasKey('Meta Data', $actual);
+        $this->assertNotEmpty($actual['Meta Data']);
+
+        $this->assertArrayHasKey('Time Series (Digital Currency Daily)', $actual);
+        $this->assertNotEmpty($actual['Time Series (Digital Currency Daily)']);
+    }
+
+    public function testDigitalCurrencyWeekly()
+    {
+        $actual = (new DigitalCurrency($this->option))->digitalCurrencyWeekly('BTC', 'CNY');
+
+        $this->assertIsArray($actual);
+        $this->assertCount(2, $actual);
+
+        $this->assertArrayHasKey('Meta Data', $actual);
+        $this->assertNotEmpty($actual['Meta Data']);
+
+        $this->assertArrayHasKey('Time Series (Digital Currency Weekly)', $actual);
+        $this->assertNotEmpty($actual['Time Series (Digital Currency Weekly)']);
+    }
+
+    public function testDigitalCurrencyMonthly()
+    {
+        $actual = (new DigitalCurrency($this->option))->digitalCurrencyMonthly('BTC', 'CNY');
+
+        $this->assertIsArray($actual);
+        $this->assertCount(2, $actual);
+
+        $this->assertArrayHasKey('Meta Data', $actual);
+        $this->assertNotEmpty($actual['Meta Data']);
+
+        $this->assertArrayHasKey('Time Series (Digital Currency Monthly)', $actual);
+        $this->assertNotEmpty($actual['Time Series (Digital Currency Monthly)']);
+    }
+}
